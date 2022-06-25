1f108841d7b248aa1f1af518d9cbeb4ef40e8d3e
Sharofiddin Azizmatov <azizmatov1202@gmail.com>
2022-06-26 02:14:32 +0300

    create and get orders

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,16 @@
+import { CreateOrderRequest } from './dto/create-order.request';
 import { Injectable } from '@nestjs/common';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly ordersRepository: OrdersRepository) {}
+
+  async createOrder(request: CreateOrderRequest) {
+    return this.ordersRepository.create(request);
+  }
+
+  async getOrders() {
+    return this.ordersRepository.find();
   }
 }
